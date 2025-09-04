cc242255062d55e8ea33ce1df6fdd1d05de20836
AssassinsCatt <andirib09@gmail.com>
2025-09-04 21:58:49 +0200

    hirn tut weh

--- meowfetch.go
+++ meowfetch.go
@@ -14,6 +14,8 @@ func main() {
   draw(file_name, id)  
 }
 
+
+
 func is_err (err error) bool{
   if err != nil {
     fmt.Println(err)
@@ -23,21 +25,36 @@ func is_err (err error) bool{
   }
 }
 
+
+
 func draw(file_name string, id int) {
   path := fmt.Sprintf("animations/%v/%v", file_name, id)
-  file, err := os.ReadFile(path)
   
-  if is_err(err) == false {
-    color.Green(string(file))
-  }else{
-    fmt.Print("\033[H\033[2J") 
-    id = 1
-    path := fmt.Sprintf("animations/%v/%v", file_name, id)
-    file, _ := os.ReadFile(path)
-    color.Green(string(file))
+  err := print(0,255,0,path)
+  
+  if err != nil {
+    draw = 1
   }
 
   time.Sleep(500 * time.Millisecond)
   fmt.Fprint(os.Stdout, "\x1b[H\x1b[2J\x1b[3J")
   draw(file_name, id+1)
+}
+
+
+
+func print(red, green, blue int, path string) (err error) {
+  file, err := os.ReadFile(path)
+  
+  if is_err(err) == false {
+    color.Green(string(file))
+
+  }else{
+    fmt.Fprint(os.Stdout, "\x1b[H\x1b[2J\x1b[3J")
+    file, _ := os.ReadFile(path)
+    color.Green(string(file))
+  }
+
+  color.RGB(red, green, blue).Println()
+  return
 }
