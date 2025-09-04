d62933143fb4f40bf346cb0da8857a533d8ca257
AssassinsCatt <andirib09@gmail.com>
2025-09-04 22:01:22 +0200

    tut weh

--- meowfetch.go
+++ meowfetch.go
@@ -18,7 +18,6 @@ func main() {
 
 func is_err (err error) bool{
   if err != nil {
-    fmt.Println(err)
     return true
   }else{
     return false
@@ -33,7 +32,7 @@ func draw(file_name string, id int) {
   err := print(0,255,0,path)
   
   if err != nil {
-    draw = 1
+    id = 1
   }
 
   time.Sleep(500 * time.Millisecond)
@@ -50,7 +49,6 @@ func print(red, green, blue int, path string) (err error) {
     color.Green(string(file))
 
   }else{
-    fmt.Fprint(os.Stdout, "\x1b[H\x1b[2J\x1b[3J")
     file, _ := os.ReadFile(path)
     color.Green(string(file))
   }
