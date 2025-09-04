18c7a3ca3041ff9ea684fe0517d71dc330d71d2c
AssassinsCatt <andirib09@gmail.com>
2025-09-04 21:24:28 +0200

    init

---
+++ meowfetch.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "os"
+)
+
+func main() {
+  file_name := "arch"
+  id := 1
+  
+  draw(file_name, id)  
+}
+
+func is_err (err error) bool{
+  if err != nil {
+    fmt.Println(err)
+    return true
+  }else{
+    return false
+  }
+}
+
+func draw(file_name string, id int) {
+  path := fmt.Sprintf("animations/%v/%v", file_name, id)
+  file, err := os.ReadFile(path)
+  
+  if is_err(err) == false {
+    fmt.Println(string(file))
+  }else{
+    fmt.Print("\033[H\033[2J") 
+    id = 1
+    path := fmt.Sprintf("animations/%v/%v", file_name, id)
+    file, _ := os.ReadFile(path)
+    fmt.Println(string(file))
+  }
+
+  time.Sleep(500 * time.Millisecond)
+  fmt.Print("\033[H\033[2J") 
+  draw(file_name, id+1)
+}
